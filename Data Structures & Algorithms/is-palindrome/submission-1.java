bf807b4581e2abacfc667c536ff36bcd55f2e940
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2026-04-28 01:25:58 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        int s = 0, e = str.length() - 1;
+        while (s < e) {
+            if (shouldSkip(str.charAt(s)))
+                s++;
+            else if (shouldSkip(str.charAt(e)))
+                e--;
+            else if (str.charAt(s) != str.charAt(e))
+                return false;
+            else {
+                s++;
+                e--;
+            }
+        }
+        return true;
+    }
+
+    boolean shouldSkip(char ch) {
+        return ( 
+            ch >= '0' && ch <='9'  || 
+            ch >= 'a' && ch <= 'z' ||
+            ch >= 'A' && ch <= 'Z'
+            ) ?  false : true;
+    }
+}
