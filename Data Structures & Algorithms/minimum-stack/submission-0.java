6e7ddfdbdb8b2ee61a207718b532987665a1f7ef
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2026-05-30 01:51:21 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if( minSt.isEmpty() || val <= minSt.peek() )
+            minSt.push( val );
+    }
+    
+    public void pop() {
+        if( st.size() == 0 )
+            return;
+
+        int ele = st.pop();
+        if( ele == minSt.peek() )
+            minSt.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return minSt.peek();
+    }
+}
